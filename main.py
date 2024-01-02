a2b2adb6e8d2e19e4302ba0585a2e798b3b14c9c
LorXinHui <xlor0001@student.monash.edu>
2024-01-02 17:06:09 +0800

    created sign_in page, and direct to sign_up for new users

--- main.py
+++ main.py
@@ -11,21 +11,35 @@ firebase_admin.initialize_app(cred, {'databaseURL': 'https://mobileproject-f497e
 #firebase_storage = storage.bucket(app = firebase_admin.get_app())
 
 @app.route('/')
+def sign_in():
+    return render_template('sign_in.html')
+
+@app.route('/signup')
 def sign_up():
     return render_template('sign_up.html')
 
-@app.route('/signup', methods = ['POST'])
+@app.route('/sign_up', methods = ['POST'])
 def signup():
     data = request.get_json()
 
-    # Check if the data already exists in the database before adding a new entry
-    users_ref = db.reference('users')
-    new_user_ref = users_ref.push({
-        'name': data.get('name'),
+    #Extract the name and replace any empty characters
+    name = data.get('name')
+    name_key = name.replace(' ', '_')
+
+    #Create a reference using the name as the key
+    users_ref = db.reference('users').child(name_key)
+
+    # Check if the user already exists
+    existing_user = users_ref.get()
+    if existing_user:
+        return jsonify({"error": "User already exists with the same name"})
+
+    #Store user data
+    new_user_ref = users_ref.set({
+        'name': name,
         'pwd': data.get('pwd'),
         'userType': data.get('userType')
     })
-
     return jsonify({"message": "Data stored successfully", "user_id": new_user_ref.key()})
 
 
@@ -36,13 +50,21 @@ def resume_submission():
     # ref.push({'text': 'Hello, Firebase!'})
     return render_template('resume_submission.html')
 
-@app.route('/messages')
-def messages():
-    # Retrieve data from Firebase
-    ref = db.reference('/messages')
-    messages = ref.get()
+@app.route('/sign_in/<username>', methods = ['GET'])
+def get_user(username):
+    # Create a reference to the 'users' node in the database
+    users_ref = db.reference('users')
 
-    return render_template('messages.html', messages=messages)
+    # Create a reference to the specific user using the provided username
+    user_ref = users_ref.child(username)
+
+    # Retrieve the user data
+    user_data = user_ref.get()
+
+    if user_data:
+        return jsonify({"user": user_data})
+    else:
+        return jsonify({"error": "User not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
