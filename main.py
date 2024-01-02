1dcea312a69e0e7f70c6f6abf91fd9d00313e1d1
LorXinHui <xlor0001@student.monash.edu>
2024-01-03 01:43:01 +0800

    Redirect different interaction to the correct webpage

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 from flask import render_template
+from flask import session, redirect, url_for
 import firebase_admin
 from firebase_admin import credentials, db, storage
 
@@ -14,6 +15,22 @@ firebase_admin.initialize_app(cred, {'databaseURL': 'https://mobileproject-f497e
 def sign_in():
     return render_template('sign_in.html')
 
+@app.route('/sign_in/<username>', methods = ['GET'])
+def signin(username):
+    # Create a reference to the 'users' node in the database
+    users_ref = db.reference('users')
+
+    # Create a reference to the specific user using the provided username
+    user_ref = users_ref.child(username)
+
+    # Retrieve the user data
+    user_data = user_ref.get()
+
+    if user_data:
+        return jsonify({"user": user_data})
+    else:
+        return jsonify({"error": "User not found"}), 404
+
 @app.route('/signup')
 def sign_up():
     return render_template('sign_up.html')
@@ -21,7 +38,6 @@ def sign_up():
 @app.route('/sign_up', methods = ['POST'])
 def signup():
     data = request.get_json()
-    print("Received data:", data)
 
     #Extract the name and replace any empty characters
     name = data.get('name')
@@ -36,36 +52,19 @@ def signup():
         return jsonify({"error": "User already exists with the same name"})
 
     #Store user data
-    new_user_ref = users_ref.push({
+    new_user_ref = users_ref.set({
         'name': name,
         'pwd': data.get('pwd'),
         'userType': data.get('userType')
     })
-    return jsonify({"message": "Data stored successfully", "user_id": new_user_ref.key()})
+    
+    return jsonify({"message": "Data stored successfully", "user_id": name_key})
 
 
 @app.route('/home')
 def resume_submission():
-    # Push data to Firebase
-    # ref = db.reference('/messages')
-    # ref.push({'text': 'Hello, Firebase!'})
     return render_template('resume_submission.html')
 
-@app.route('/sign_in/<username>', methods = ['GET'])
-def get_user(username):
-    # Create a reference to the 'users' node in the database
-    users_ref = db.reference('users')
-
-    # Create a reference to the specific user using the provided username
-    user_ref = users_ref.child(username)
-
-    # Retrieve the user data
-    user_data = user_ref.get()
-
-    if user_data:
-        return jsonify({"user": user_data})
-    else:
-        return jsonify({"error": "User not found"}), 404
 
 if __name__ == "__main__":
     app.run(debug=True)
